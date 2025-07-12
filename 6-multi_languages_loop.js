5807022ab95727462ca9c4eb8142e403058d1e02
mustaphaisml97 <mustaphaisml93@gmail.com>
2025-07-12 14:09:40 +0100

    6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -9,8 +9,4 @@ const lines = [
 for (const line of lines) {
   console.log(line);
 }
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-while (i < lines.length) {
-  console.log(lines[i++]);
-}
+
