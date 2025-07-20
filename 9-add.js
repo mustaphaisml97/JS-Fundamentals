0c495ac7b61259e2572fa1ee44e221274e11a3fa
mustaphaisml97 <mustaphaisml93@gmail.com>
2025-07-20 11:17:02 +0100

    9-add.js

--- 9-add.js
+++ 9-add.js
@@ -2,11 +2,8 @@ function add(a, b) {
   return a + b;
 }
 
-const num1 = Number(process.argv[2]);
-const num2 = Number(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('Please provide two valid numbers');
-} else {
-  console.log(add(num1, num2));
-}
+console.log(add(a, b));
+
